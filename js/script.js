c8b4d14b09733a051d02f380ac03099dc4e22316
SoraK93 <176456696+SoraK93@users.noreply.github.com>
2025-09-14 13:41:27 +0530

    COMPLETE TABLE FORMAT AND ROW CREATION

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 const library = [];
 
+// Book
 function Book(title, author, pages, read) {
   this.id = getUUID();
   this.title = title;
@@ -25,8 +26,58 @@ function addToLibrary(newBook) {
   return library;
 }
 
+// Utility
+function capitalize(text) {
+  return text[0].toUpperCase() + text.slice(1);
+}
+
+function createBookRow(parentElement, elementName, elementClass) {
+  const book = document.createElement(elementName);
+  book.className = elementClass;
+  parentElement.appendChild(book);
+  return book;
+}
+
+// Table
+function createBookColumn(row) {
+  // book row
+  const bookList = document.querySelector("#bookList");
+  const bookID = `book`;
+  row(bookList, "tr", bookID);
+
+  // book serial number
+  const book = document.getElementsByClassName(bookID);
+  console.log(book[0]);
+  row(book[0], "th", "serial");
+
+  // create elements for book details
+  childClassList = [
+    "bookTitle",
+    "bookAuthor",
+    "bookPages",
+    "bookRead",
+    "update",
+  ];
+
+  childClassList.forEach((element) => {
+    row(book[0], "th", element);
+  });
+
+  // create buttons
+  ["change", "remove"].forEach((element) => {
+    const update = document.querySelector(".update");
+    row(update, "button", element).innerText = capitalize(element);
+  });
+}
+
 addToLibrary(createBook("The Hobbit", "J.R.R. Tolkien", 295, "Not Read"));
-addToLibrary(createBook("Practical Object-Oriented Design", "Sandi Metz", 288, "Not Read"))
-addToLibrary(createBook("The Practicing Mind", "Thomas M. Sterner", 164, "Not Read"))
-addToLibrary(createBook("The Richest Man in Babylon", "George S. Clason", 128, "Read"))
-console.log(library[3].info());
+addToLibrary(
+  createBook("Practical Object-Oriented Design", "Sandi Metz", 288, "Not Read")
+);
+addToLibrary(
+  createBook("The Practicing Mind", "Thomas M. Sterner", 164, "Not Read")
+);
+addToLibrary(
+  createBook("The Richest Man in Babylon", "George S. Clason", 128, "Read")
+);
+createBookColumn(createBookRow);
